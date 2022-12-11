1b91ee98edd47b1acd231a2567ad8f8e7ea94821
imran <imran40031@gmail.com>
2022-12-11 17:10:01 +0600

    Initial Commit

---
+++ Swift_list/Swift_listApp.swift
@@ -0,0 +1,17 @@
+//
+//  Swift_listApp.swift
+//  Swift_list
+//
+//  Created by md imran on 11/12/22.
+//
+
+import SwiftUI
+
+@main
+struct Swift_listApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
